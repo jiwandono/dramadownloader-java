e0cd89f37cecb90c2b2975327640f3ea80ca9aa4
Adityo Jiwandono <jiwandono@gmail.com>
2015-09-27 13:59:49 +0700

    Add getter/setter to LegacyMonitorEntry id field.

--- src/main/java/com/dramadownloader/app/api/v1/monitor/LegacyMonitorEntry.java
+++ src/main/java/com/dramadownloader/app/api/v1/monitor/LegacyMonitorEntry.java
@@ -19,6 +19,14 @@ public class LegacyMonitorEntry {
     this.success = success;
   }
 
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
   public String getUrl() {
     return url;
   }
