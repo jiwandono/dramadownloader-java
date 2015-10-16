fdd58f0a2177d8d1a5b20425910349dfd864f675
Adityo Jiwandono <adityo@traveloka.com>
2015-10-16 22:14:45 +0700

    Fix videoupload.us scraper.

--- src/main/java/com/dramadownloader/scraper/file/VideouploadusFileScraper.java
+++ src/main/java/com/dramadownloader/scraper/file/VideouploadusFileScraper.java
@@ -1,5 +1,9 @@
 package com.dramadownloader.scraper.file;
 
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
@@ -21,18 +25,37 @@ public class VideouploadusFileScraper extends FileScraper {
 
     if(url.contains("videoupload.us/drama/video")) {
       // Leave as is.
-      result.setStatus(FileScrapeResult.Status.OK);
       result.getFiles().add(new FileScrapeResult.File(url, false));
     } else if(url.contains("videoupload.us/drama/embed-")) {
       String downloadUrl = url.replace("/embed-", "/video-");
-
-      result.setStatus(FileScrapeResult.Status.OK);
       result.getFiles().add(new FileScrapeResult.File(downloadUrl, false));
     } else if(url.contains("videoupload.us/embed/drama-")) {
       String downloadUrl = url.replace("/embed/drama-", "/drama/video-");
-
-      result.setStatus(FileScrapeResult.Status.OK);
       result.getFiles().add(new FileScrapeResult.File(downloadUrl, false));
+    } else {
+      // Look in the script tag. Actually there are multiple streams, but maybe later...
+      Set<String> fileSet = new HashSet<>();
+      Elements scripts = getDocument(url).getElementsByTag("script");
+      for(Element script : scripts) {
+        for(DataNode dataNode : script.dataNodes()) {
+          String actualScript = dataNode.getWholeData();
+          String nospaces = actualScript.replace(" ", "");
+          int pos1 = nospaces.indexOf("file:'");
+          int pos2 = nospaces.indexOf("'", pos1 + 6);
+          if(pos1 != -1 && pos2 != -1 && pos2 > pos1) {
+            String downloadUrl = nospaces.substring(pos1 + 6, pos2);
+            fileSet.add(downloadUrl);
+          }
+        }
+      }
+
+      for(String downloadUrl : fileSet) {
+        result.getFiles().add(new FileScrapeResult.File(downloadUrl, true));
+      }
+    }
+
+    if(result.getFiles().size() > 0) {
+      result.setStatus(FileScrapeResult.Status.OK);
     }
 
     return result;
